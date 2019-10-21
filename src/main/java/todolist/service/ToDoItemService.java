e2a2f20b69d71558c53072b4e9e3999f7c986fb1
Roland <rolandattila@yahoo.com>
2019-10-21 21:36:10 +0300

    added createToDoItem endpoint

---
+++ src/main/java/todolist/service/ToDoItemService.java
@@ -0,0 +1,35 @@
+package todolist.service;
+
+import todolist.domain.ToDoItem;
+import todolist.persistance.ToDoItemRepository;
+import todolist.transfer.CreateToDoItemRequest;
+import todolist.transfer.UpdateToDoItemRequest;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ToDoItemService {
+
+    private ToDoItemRepository toDoItemRepository = new ToDoItemRepository();
+
+    public void createToDoItem(CreateToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Creating to-do-item: " + request);
+        toDoItemRepository.createToDoItem(request);
+    }
+
+    public void updateToDoItem(long id, UpdateToDoItemRequest request) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Updating to-do-item: " + request);
+        toDoItemRepository.updateToDoUpdate(request.isDone(), id);
+    }
+
+    public void deleteToDoItem(long id) throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Deleting item: " + id);
+        toDoItemRepository.deleteToDoItem(id);
+    }
+
+    public List<ToDoItem> getToDoItems() throws SQLException, IOException, ClassNotFoundException {
+        System.out.println("Retrieving to-do-items....");
+        return toDoItemRepository.getToDoItems();
+    }
+}
